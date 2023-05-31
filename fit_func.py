653e4de59c17452eecf809330d101544c68bbd39
Yue Zhang <yuez11@illinois.edu>
2023-05-31 11:20:08 -0500

    Update fit_func.py

--- fit_func.py
+++ fit_func.py
@@ -12,4 +12,4 @@ from statsmodels.nonparametric.smoothers_lowess import lowess
 def fit(spectra,peak_num,sm_frac):
     filtered = lowess(spectra[1].values, spectra[1].index, is_sorted=True, frac=sm_frac, it=0).T
     peaks, dict = find_peaks(filtered[1], height=0)
-    return np.sort(filtered[0][peaks[np.argpartition(dict['peak_heights'],-3)[-3:]]])
+    return np.sort(filtered[0][peaks[np.argpartition(dict['peak_heights'],-peak_num)[-peak_num:]]])
